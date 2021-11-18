e470752b41fb82963e53673e22767d951a7a8cc8
Kevin <polopolopolopoline@gmail.com>
2021-11-17 22:09:48 -0600

    cambios

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_application_1/pages/rutas.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -12,62 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Welcome to Flutter',
       home: Scaffold(
-        
-        body: WebView(
-          initialUrl: "https://xidoo.netlify.app/",
-          javascriptMode: JavascriptMode.unrestricted,
-          ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-          floatingActionButton: _crearBotones(),
-      ),
+        body: Rutas(),
+      )
     );
   }
 }
 
-Widget _crearBotones(){
-
-    return  Container(
-
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(27),
-        color: Colors.black87,
-      ),
-      margin: EdgeInsets.all(10),
-      height: 80,
-      width: 700,
-
-      child: Row(
-
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-
-      FloatingActionButton(
-        child: Icon(Icons.account_circle_rounded, color: Colors.black,),
-          onPressed: null,
-            backgroundColor: Colors.amber,),
-              SizedBox(width: 10.0,),
-      FloatingActionButton (
-        child: Icon(Icons.shopping_basket, color: Colors.black,),
-          onPressed: null,
-           backgroundColor: Colors.amber,),
-              SizedBox(width: 10.0,),
-      FloatingActionButton (
-        child: Icon(Icons.directions_bus, color: Colors.black,),
-          onPressed: null,
-            backgroundColor: Colors.amber,),
-              SizedBox(width: 10.0,),
-      FloatingActionButton (
-        child: Icon(Icons.museum, color: Colors.black,),
-          onPressed: null,
-            backgroundColor: Colors.amber,),
-              SizedBox(width: 10.0,),
-      FloatingActionButton (
-          child: Icon(Icons.message, color: Colors.black,),
-            onPressed: null,
-              backgroundColor: Colors.amber,),
-
-      ]),
-    );
-}
